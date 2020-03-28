a5b7b26623a76aa874737a1f176d0ce1101eae6c
Rohmats <rohmatsuseno@gmail.com>
2020-03-28 10:40:33 +0700

    First working app, added cocoaPods

---
+++ IGListExample/ViewControllers/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  IGListExample
+//
+//  Created by Rohmat Suseno on 28/03/20.
+//  Copyright © 2020 Rohmts. All rights reserved.
+//
+
+import UIKit
+
+class MainViewConroller: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
---
+++ Pods/Target Support Files/Pods-IGListExample/Pods-IGListExample-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_IGListExample : NSObject
+@end
+@implementation PodsDummy_Pods_IGListExample
+@end
